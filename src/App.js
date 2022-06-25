abc3e8143cc94626b574ec30ef25e416ef778958
JessieZevaka <101160859+JessieZevaka@users.noreply.github.com>
2022-06-25 15:31:49 +1000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jess Zevaka and is{" "}
-        <a
-          href="https://github.com/JessieZevaka/weather-app-example"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jess Zevaka and is{" "}
+          <a
+            href="https://github.com/JessieZevaka/weather-app-example"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
